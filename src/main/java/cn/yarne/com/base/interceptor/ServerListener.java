93bb6f6b0d8e8fdd6655388e41c532e9c6233626
yarne <y_nell@163.com>
2018-05-21 17:56:41 +0800

    配置自定义监听器

---
+++ src/main/java/cn/yarne/com/base/interceptor/ServerListener.java
@@ -0,0 +1,26 @@
+package cn.yarne.com.base.interceptor;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class ServerListener implements ServletContextListener {
+	protected final Logger logger = LogManager.getLogger(this.getClass());
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		logger.info("=================================");
+		logger.info("项目[{"+arg0.getServletContext().getServletContextName()+"}]已经销毁!!!");
+		logger.info("=================================");
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		logger.info("=================================");
+		logger.info("项目[{"+arg0.getServletContext().getServletContextName()+"}]启动完成!!!");
+		logger.info("=================================");
+	}
+
+}
