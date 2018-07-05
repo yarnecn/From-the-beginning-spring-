21f8e3b286f2145eec94cfbeac0143b5d4865605
yarne <y_nell@163.com>
2018-07-05 22:19:00 +0800

    index 首页布局zz

--- src/main/java/cn/yarne/com/base/shiro/controller/IndexController.java
+++ src/main/java/cn/yarne/com/base/shiro/controller/IndexController.java
@@ -1,7 +1,12 @@
 package cn.yarne.com.base.shiro.controller;
 
+import cn.yarne.com.base.model.SysUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by 14641 on 2018/6/9.
@@ -10,7 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     @RequestMapping(value = "index")
-    public String index(){
+    public String index(HttpServletRequest request,Model model){
+        HttpSession session = request.getSession();
+        SysUser sysUser = (SysUser) session.getAttribute("sysUser");
+        model.addAttribute("sysUser",sysUser);
         //默认跳转到用户登录页面
         return "index";
     }
--- src/main/java/cn/yarne/com/base/shiro/filter/ShiroFormFilter.java
+++ src/main/java/cn/yarne/com/base/shiro/filter/ShiroFormFilter.java
@@ -1,5 +1,6 @@
 package cn.yarne.com.base.shiro.filter;
 
+import cn.yarne.com.base.model.SysUser;
 import org.apache.log4j.Logger;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.subject.Subject;
@@ -8,6 +9,8 @@ import org.apache.shiro.web.util.WebUtils;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * shiro认证成功之后的拦截处理
@@ -19,6 +22,13 @@ public class ShiroFormFilter extends FormAuthenticationFilter {
     @Override
     protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request, ServletResponse response) throws Exception {
         logger.debug("用户登录成功之后的处理");
+        //获取已登录的用户信息
+        SysUser activeUser = (SysUser) subject.getPrincipal();
+        //获取session
+        HttpServletRequest httpServletRequest = WebUtils.toHttp(request);
+        HttpSession session = httpServletRequest.getSession();
+        //把用户信息保存到session
+        session.setAttribute("sysUser", activeUser);
         //shiro权限框架在认证时认证通过之后 就会跳转到上一次访问的路径
         //所以我们调用这个方法清除之前保存的请求地址
         WebUtils.getAndClearSavedRequest(request);
