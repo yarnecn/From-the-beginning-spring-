ddd84fa335f7c2f5525e0413a22828db2e2054b8
yarne <y_nell@163.com>
2018-07-09 17:02:46 +0800

    集成验证码zzz

---
+++ src/main/java/cn/yarne/com/base/shiro/filter/ValidateCodeFilter.java
@@ -0,0 +1,60 @@
+package cn.yarne.com.base.shiro.filter;
+
+import org.apache.log4j.Logger;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.web.filter.AccessControlFilter;
+import org.apache.shiro.web.util.WebUtils;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by 14641 on 2018/7/9.
+ */
+public class ValidateCodeFilter extends AccessControlFilter {
+
+    private static final Logger logger = Logger.getLogger(ValidateCodeFilter.class);
+
+    private boolean jcaptchaEbabled = true;//是否开启验证码支持
+    private String jcaptchaParam = "verCode";//前台提交的验证码参数名
+    private String failureKeyAttribute = "shiroLoginFailure"; //验证失败后存储到的属性名
+
+    public void setJcaptchaEbabled(boolean jcaptchaEbabled) {
+        this.jcaptchaEbabled = jcaptchaEbabled;
+    }
+    public void setJcaptchaParam(String jcaptchaParam) {
+        this.jcaptchaParam = jcaptchaParam;
+    }
+    public void setFailureKeyAttribute(String failureKeyAttribute) {
+        this.failureKeyAttribute = failureKeyAttribute;
+    }
+    @Override
+    protected boolean isAccessAllowed(ServletRequest servletRequest, ServletResponse servletResponse, Object o) throws Exception {
+        logger.debug("判断验证码是否正确");
+        //1、设置验证码是否开启属性，页面可以根据该属性来决定是否显示验证码
+        servletRequest.setAttribute("jcaptchaEbabled", jcaptchaEbabled);
+        HttpServletRequest httpServletRequest = WebUtils.toHttp(servletRequest);
+       //2、判断验证码是否禁用 或不是表单提交（允许访问）
+        if (jcaptchaEbabled == false
+                || !"post".equalsIgnoreCase(httpServletRequest.getMethod())) {
+            return true;
+        }
+       //3、此时是表单提交，验证验证码是否正确
+        Session session = SecurityUtils.getSubject().getSession();
+        String code = (String) session.getAttribute("code");
+        if(code!=null){
+            return code.equals(jcaptchaParam);
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
+        logger.debug("验证码错误");
+        //如果验证码失败了，存储失败 key 属性
+        servletRequest.setAttribute(failureKeyAttribute, "CodeError");
+        return true;
+    }
+}
